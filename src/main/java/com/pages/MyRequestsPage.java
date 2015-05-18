9d91eaad752d52e73bc588cba629ae8d689ecef4
razvancuceu <razvancuceu@EVO492.evointern>
2015-05-18 14:08:26 +0300

    Added tests for Vacation Type checkboxex from My Requests page

--- src/main/java/com/pages/MyRequestsPage.java
+++ src/main/java/com/pages/MyRequestsPage.java
@@ -17,12 +17,66 @@ public class MyRequestsPage extends PageObject {
 
 	@FindBy(css="a[href*='menuItem=my-requests']")
 	private WebElementFacade MyRequests;
-	  
-	  
+	 
+	@FindBy(id="_evovacation_WAR_EvoVacationportlet_applyButton")
+	private WebElementFacade ApplyButton;
+	
+	@FindBy(id="_evovacation_WAR_EvoVacationportlet_HOLIDAYCheckbox")
+	private WebElementFacade Holiday;
+	
+	@FindBy(id="_evovacation_WAR_EvoVacationportlet_VACATION_WITHOUT_PAYMENTCheckbox")
+	private WebElementFacade WithoutPayment;
+	
+	@FindBy(id="_evovacation_WAR_EvoVacationportlet_SPECIAL_VACATIONCheckbox")
+	private WebElementFacade SpecialVacation;
+	
+	@FindBy(id="_evovacation_WAR_EvoVacationportlet_SICK_LEAVECheckbox")
+	private WebElementFacade SickLeave;
+	
+	@FindBy(id="_evovacation_WAR_EvoVacationportlet_MATERNITY_LEAVECheckbox")
+	private WebElementFacade MaternityLeave;
+	
 	public void selectMyRequests(){
-		MyRequests.click();
-	    	
+		MyRequests.click();	    	
 	    }
 	
+	public void selectApplyButton(){
+		ApplyButton.click();	    	
+	    }
+	
+	public void selectHoliday(){
+		Holiday.click();	    	
+	    }
+	
+	public void selectWithoutPayment(){
+		WithoutPayment.click();	    	
+	    }
+	
+	public void selectSpecialVacation(){
+		SpecialVacation.click();	    	
+	    }
+	
+	public void selectSickLeave(){
+		SickLeave.click();	    	
+	    }
+	
+	public void selectMaternityLeave(){
+		MaternityLeave.click();	    	
+	    }
+	
+		
+	  public List<String> getType() {
+		  WebElementFacade TypeList = find(By.className("taglib-search-iterator"));		  
+	      List<WebElement> results = TypeList.findElements(By.xpath("//*[contains(@id, '_evovacation_WAR_EvoVacationportlet_evozonVacationsSearchContainer_col-my.request.column.header.type')]"));
+	        return convert(results, toStrings());
+	    }
+
+	    private Converter<WebElement, String> toStrings() {
+	        return new Converter<WebElement, String>() {
+	            public String convert(WebElement from) {
+	                return from.getText();
+	            }
+	        };
+	    }
 	
 }
